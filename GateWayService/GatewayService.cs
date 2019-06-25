69b391a3dd7311b8937c0d8249388e3b4889d98c
英璟 陳 <e30803@hotmail.com>
2019-06-25 18:23:14 +0800

    加入專案檔案。

---
+++ GateWayService/GatewayService.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using Kernel.Interface;
+using Kernel.Common;
+using Kernel.QueueManager;
+using ObjectManager;
+using System.Collections.Concurrent;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+
+namespace GatewayService
+{
+    public class GatewayService : IService, IDisposable
+    {
+        private ConcurrentQueue<cls_ProcRecv_CollectData> _Update_ProcRecv_CollectData_Queue = new ConcurrentQueue<cls_ProcRecv_CollectData>();
+        public delegate void Put_ProcRecv_CollectData_Event(cls_ProcRecv_CollectData obj_CollectData);
+
+        public Put_ProcRecv_CollectData_Event Put_ProcRecv_CollectData;
+
+        private string _SeviceName = "GatewayService";
+        public string ServiceName
+        {
+            get
+            {
+                return this._SeviceName;
+            }
+
+        }
+
+        private readonly IQueueManager _QueueManager;
+        private readonly IManagement _ObjectManager;
+        private readonly ILogger<GatewayService> _logger;
+
+        private ObjectManager.ObjectManager _objectmanager = null;
+
+        private Thread _th_Proc_Flow =null;
+        private System.Threading.Timer timer_heartbet;
+        private bool _run = false;
+
+
+
+        public GatewayService(ILoggerFactory loggerFactory,IServiceProvider serviceProvider)
+        {
+            _QueueManager = serviceProvider.GetService<IQueueManager>();
+            _ObjectManager = serviceProvider.GetServices<IManagement>().Where(o => o.ManageName.Equals("ObjectManager")).FirstOrDefault();
+            _logger = loggerFactory.CreateLogger<GatewayService>();
+        }
+
+
+        public void Init()
+        {
+            _objectmanager = (ObjectManager.ObjectManager)_ObjectManager.GetInstance;
+            
+            this._run = true;
+            this._th_Proc_Flow = new Thread(new ThreadStart(Proc_Flow));
+            this._th_Proc_Flow.Start();
+
+            Timer_Report_HeartBet(60);
+
+
+        }
+
+        public void Dispose()
+        {
+            _run = false;
+        }
+
+
+        private void Timer_Report_HeartBet(int interval)
+        {
+            if (interval == 0)
+                interval = 10000;  // 10s
+
+            //使用匿名方法，建立帶有參數的委派
+            System.Threading.Thread Thread_Timer_Report_EDC = new System.Threading.Thread
+            (
+               delegate (object value)
+               {
+                   int Interval = Convert.ToInt32(value);
+                   timer_heartbet = new System.Threading.Timer(new System.Threading.TimerCallback(HeartBet_TimerTask), null, 1000, Interval);
+               }
+            );
+            Thread_Timer_Report_EDC.Start(interval);
+        }
+
+        private void HeartBet_TimerTask(object timerState)
+        {
+            try
+            {
+               
+
+               
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+
+
+
+        public void Proc_Flow()
+        {
+            while (_run)
+            {
+                
+                if (_Update_ProcRecv_CollectData_Queue.Count > 0)
+                {
+                    cls_ProcRecv_CollectData _msg = null;
+                    while (_Update_ProcRecv_CollectData_Queue.TryDequeue(out _msg))
+                    {
+                        string Gateway_ID = _msg.GateWayID;
+                        string Device_ID = _msg.Device_ID;
+                       
+                        // Update Normal Tag
+                        UpdateGatewayTagInfo(_msg);
+
+                        // Update Calculate Tag
+                        // ProcCalcTag(Gateway_ID, Device_ID);
+
+                        // 結合EDC and other Information 送MQTT
+                        Organize_EDCPartaker(Gateway_ID, Device_ID);
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+
+        public void UpdateGatewayTagInfo(cls_ProcRecv_CollectData ProcData)
+        {
+            _objectmanager.GatewayManager_Set_TagValue(ProcData);
+        }
+
+
+        public void Organize_EDCPartaker(string GateWayID, string Device_ID)
+        {
+            //--- 等待EDC List information 
+
+            if (_objectmanager.EDCManager == null)
+                return;
+
+            List<ObjectManager.cls_EDC_Info> lst_EDCInfo = _objectmanager.EDCManager.gateway_edc.Where(p => p.gateway_id == GateWayID && p.device_id == Device_ID && p.enable == true).ToList();
+
+            foreach (cls_EDC_Info _EDC in lst_EDCInfo)
+            {
+                EDCPartaker EDCReporter = new EDCPartaker(_EDC);
+                EDCReporter.timestapm = DateTime.Now;
+
+                cls_Gateway_Info gateway = _objectmanager.GatewayManager.gateway_list.Where(p => p.gateway_id == GateWayID).FirstOrDefault();
+                if (gateway != null)
+                {
+                    cls_Device_Info device = gateway.device_info.Where(p => p.device_name == Device_ID).FirstOrDefault();
+                    if (device != null)
+                    {
+                        // Assembly Normal Tag info
+                        foreach (Tuple<string, string> _Items in _EDC.tag_info)
+                        {
+                            cls_EDC_Body_Item edctiem = new cls_EDC_Body_Item();
+                            edctiem.item_name = _Items.Item1;
+                            edctiem.item_type = "X";
+                            if (device.tag_info.ContainsKey(_Items.Item2))
+                                edctiem.item_value = device.tag_info[_Items.Item2].Value;
+                            else
+                                edctiem.item_value = string.Empty;
+
+                            EDCReporter.edcitem_info.Add(edctiem);
+                        }
+
+                        // Assembly Calc Tag info
+                        foreach (Tuple<string, string> _Items in _EDC.calc_tag_info)
+                        {
+                            cls_EDC_Body_Item edctiem = new cls_EDC_Body_Item();
+                            edctiem.item_name = _Items.Item1;
+                            edctiem.item_type = "X";
+
+                            if (device.calc_tag_info.ContainsKey(_Items.Item2))
+                                edctiem.item_value = device.calc_tag_info[_Items.Item2].Value;
+                            else
+                                edctiem.item_value = string.Empty;
+
+                            EDCReporter.edcitem_info.Add(edctiem);
+                        }
+                    }
+                }
+
+                //----- Send MQTT-----
+                xmlMessage SendOutMsg = new xmlMessage();
+                SendOutMsg.LineID = GateWayID;     // GateID
+                SendOutMsg.DeviceID = Device_ID;   // DeviceID
+                SendOutMsg.MQTTTopic = "EDCService";
+                SendOutMsg.MQTTPayload = JsonConvert.SerializeObject(EDCReporter, Newtonsoft.Json.Formatting.Indented);
+                _QueueManager.PutMessage(SendOutMsg);
+            }
+        }
+
+
+
+        public void _Update_ProcRecv_CollectData(cls_ProcRecv_CollectData obj_CollectData)
+        {
+            this._Update_ProcRecv_CollectData_Queue.Enqueue(obj_CollectData);
+        }
+
+        public void ReceiveMQTTData(xmlMessage InputData)
+        {
+            // Parse Mqtt Topic
+            string[] Topic = InputData.MQTTTopic.Split('/');    // /IEW/GateWay/Device/ReplyData
+            string GateWayID = Topic[2].ToString();
+            string DeviceID = Topic[3].ToString();
+
+            _logger.LogInformation(string.Format("GateWay Service Handle Receive Data GateWayID = {0}, DeviceID = {1}, Topic = {2}.", GateWayID, DeviceID, InputData.MQTTTopic));
+            _logger.LogDebug(string.Format("GateWay Service Handle Receive Data  Topic = {0}, Payload = {1}.",  InputData.MQTTTopic, InputData.MQTTPayload));
+
+            if (_objectmanager.GatewayManager != null)
+            {
+                cls_Gateway_Info Gateway = _objectmanager.GatewayManager.gateway_list.Where(p => p.gateway_id == GateWayID).FirstOrDefault();
+                if (Gateway != null)
+                {
+                    cls_Device_Info Device = Gateway.device_info.Where(p => p.device_name == DeviceID).FirstOrDefault();
+                    if (Device != null)
+                    {
+                        try
+                        {
+                            ProcCollectData Function = new ProcCollectData(Device, GateWayID, DeviceID, _Update_ProcRecv_CollectData);
+                            ThreadPool.QueueUserWorkItem(o => Function.ThreadPool_Proc(InputData.MQTTPayload.ToString()));
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError("Gateway Services Handle Receive MQTT Message Error :" + ex.Message);
+                           
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ ObjectManager/ObjectManager.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Linq;
+using Kernel.Interface;
+using Kernel.Common;
+using Kernel.MQTTManager;
+using Kernel.QueueManager;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+namespace ObjectManager
+{
+
+
+    public class ObjectManager :IManagement
+    {
+        private string _ManageName = "ObjectManager";
+
+        private readonly ILogger<ObjectManager> _logger;
+        public ObjectManager(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ObjectManager>();
+           
+        }
+
+        public GateWayManager GatewayManager = null;
+        public TagSetManager TagSetManager = null;
+        public EDCManager EDCManager = null;
+        public MonitorManager MonitorManager = null;
+
+
+        public string ManageName
+        {
+            get
+            {
+                return this._ManageName;
+            }
+           
+        }
+
+        public object GetInstance
+        {
+            get
+            {
+                return this;
+            }
+
+        }
+
+        public void Init()
+        {
+            string _config_path = @"C:\\Gateway\\Config\\Gateway_Device_Config.json";
+            if ( !LoadGatewayConfig(_config_path))
+            {
+                _logger.LogError("Config File not exist, Path = " + _config_path);
+
+            }
+
+            string _config_EDC_path = @"C:\\Gateway\\Config\\EDC_Xml_Config.json";
+            if (!LoadEDCXmlConfig(_config_EDC_path))
+            {
+                _logger.LogError("EDC File not exist, Path = " + _config_path);
+
+            }
+
+        }
+
+
+        private bool LoadGatewayConfig(string config_path)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(config_path))
+                {
+                    GatewayManager_Initial();
+                    return true;
+                }
+
+                StreamReader inputFile = new StreamReader(config_path);
+                string json_string = inputFile.ReadToEnd();
+                GatewayManager_Initial(json_string);
+
+                if (GatewayManager.gateway_list == null)
+                {
+                    _logger.LogWarning("No gateway exists!");
+                    return false;
+                }
+
+                inputFile.Close();
+            }
+            catch
+            {
+                _logger.LogError("Gateway config file loading error!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool LoadEDCXmlConfig(string config_EDC_path)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(config_EDC_path))
+                {
+                  
+                    EDCManager_Initial();
+                    return true;
+                }
+
+                StreamReader inputFile = new StreamReader(config_EDC_path);
+
+                string json_string = inputFile.ReadToEnd();
+
+                EDCManager_Initial(json_string);
+
+                if (EDCManager.gateway_edc == null)
+                {
+                   _logger.LogWarning("No EDC XML config exists!");
+                    return false;
+                }
+
+                inputFile.Close();
+            }
+            catch
+            {
+                _logger.LogError("EDC XML config file loading error!!");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+        #region Gateway Constructor
+        public void GatewayManager_Initial()
+        {
+            this.GatewayManager = new GateWayManager();
+        }
+
+        public void GatewayManager_Initial(string Json)
+        {
+            this.GatewayManager = JsonConvert.DeserializeObject<GateWayManager>(Json);
+        }
+        #endregion
+
+        #region Tagset Constructor
+        public void TagSetManager_Initial()
+        {
+            this.TagSetManager = new TagSetManager();
+        }
+
+        public void TagSetManager_Initial(string Json)
+        {
+            this.TagSetManager = JsonConvert.DeserializeObject<TagSetManager>(Json);
+        }
+        #endregion
+
+        #region EDCManager Constructor
+        public void EDCManager_Initial()
+        {
+            this.EDCManager = new EDCManager();
+        }
+
+        public void EDCManager_Initial(string Json)
+        {
+            this.EDCManager = JsonConvert.DeserializeObject<EDCManager>(Json);
+        }
+        #endregion
+
+        #region MonitorManager Constructor
+        public void MonitorManager_Initial()
+        {
+            this.MonitorManager = new MonitorManager();
+        }
+
+        public void MonitorManager_Initial(string Json)
+        {
+            this.MonitorManager = JsonConvert.DeserializeObject<MonitorManager>(Json);
+        }
+        #endregion
+
+        #region Gateway Method
+        public void GatewayManager_Set_TagValue(cls_ProcRecv_CollectData ProcRecv_CollectData)
+        {
+            string _GateWayID = ProcRecv_CollectData.GateWayID;
+            string _DeviceID = ProcRecv_CollectData.Device_ID;
+
+            cls_Gateway_Info Gateway = GatewayManager.gateway_list.Where(p => p.gateway_id == _GateWayID).FirstOrDefault();
+            if (Gateway != null)
+            {
+                cls_Device_Info Device = Gateway.device_info.Where(p => p.device_name == _DeviceID).FirstOrDefault();
+                if (Device != null)
+                {
+                    Tuple<string, string> _tag = null;
+                    while (ProcRecv_CollectData.Prod_EDC_Data.TryDequeue(out _tag))
+                    {
+                        if (Device.tag_info.ContainsKey(_tag.Item1))
+                        {
+                            cls_Tag tag = Device.tag_info[_tag.Item1];
+                            tag.Value = _tag.Item2;
+                            Device.tag_info.AddOrUpdate(_tag.Item1, tag, (key, oldvalue) => tag);
+                        }
+                    }
+                }
+                else
+                {
+                 //   NLogManager.Logger.LogError("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name, string.Format("Device {0} Not Exist, So Skip Update Tag Value", _DeviceID));
+                }
+            }
+            else
+            {
+             //   NLogManager.Logger.LogError("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name, string.Format("Gateway {0} Not Exist, So Skip Update Tag Value", _GateWayID));
+
+            }
+
+
+        }
+        public string GatewayManager_ToJson_String()
+        {
+
+            try
+            {
+                return JsonConvert.SerializeObject(GatewayManager, Newtonsoft.Json.Formatting.Indented);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public string TagSetManager_ToJson_String()
+        {
+
+            try
+            {
+                return JsonConvert.SerializeObject(TagSetManager, Newtonsoft.Json.Formatting.Indented);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public string MonitorManager_ToJson_String()
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(MonitorManager, Newtonsoft.Json.Formatting.Indented);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public string GatewayCommand_Json(string _Cmd_Type, string _Report_Interval, string _Trace_ID, string _GateWayID, string _DeviceID)
+        {
+            cls_Gateway_Info Gateway = GatewayManager.gateway_list.Where(p => p.gateway_id == _GateWayID).FirstOrDefault();
+            if (Gateway == null)
+            {
+                return null;
+            }
+            else
+            {
+                cls_Device_Info Device = Gateway.device_info.Where(p => p.device_name == _DeviceID).FirstOrDefault();
+                if (Device == null)
+                {
+                    return null;
+                }
+                else
+                {
+
+                    cls_Collect collect_cmd = new cls_Collect(_Cmd_Type, _Report_Interval, _Trace_ID, Device);
+                    return JsonConvert.SerializeObject(collect_cmd, Newtonsoft.Json.Formatting.Indented);
+                }
+            }
+        }
+        #endregion
+
+
+    }
+}
